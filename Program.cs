cd4bcb292986aa06469d8c9bb498b5156a8351c0
seancrossettie <seancrossettie@gmail.com>
2021-08-03 15:28:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var goodSongs = new List<Song>();
+            var allSongs = new Song[12];
+
+            var TakeMeHigher = new Song("Creed", "Take Me Higher");
+            var LipsOfAnAngel = new Song("Hinder", "Lips of An Angel");
+            var SecondChance = new Song("Shinedown", "Second Chance");
+            var ItsNotOver = new Song("Daughtry", "It's Not Over");
+            var WithArmsWideOpen = new Song("Creed", "With Arms Wide Open");
+            var Rockstar = new Song("Post Malone", "Rockstart");
+            var Otherside = new Song("Red Hot Chili Peppers", "Otherside");
+            var NoOneKnows = new Song("Queens of the Stone Age", "No One Knows");
+            var MySacrifice = new Song("Creed", "My Sacrifice");
+            var Alive = new Song("Pearl Jam", "Alive");
+            var ManInTheBox = new Song("Alice in Chains", "Man in the Box");
+            var InBloom = new Song("Nirvana", "In Bloom");
+        }
+    }
+}
